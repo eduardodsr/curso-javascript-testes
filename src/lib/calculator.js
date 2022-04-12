cf3922e63371298b3b48584b0de41cbb8eafbe23
Eduardo Rodrigues <eduardodsr@gmail.com>
2022-04-12 01:50:47 -0300

    🧪  test calculator.js

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,12 +1,12 @@
 module.exports.sum = (num1, num2) => {
-    const int1 = parseInt(num1, 10);
-    const int2 = parseInt(num2, 10);
+  const int1 = parseInt(num1, 10);
+  const int2 = parseInt(num2, 10);
 
-    // console.log(int1, int2);
+  // console.log(int1, int2);
 
-    if (Number.isNaN(int1) || Number.isNaN(int2)) {
-        throw new Error('Please check your input. Cannot sum non-numeric values');
-    }
-    
-    return +int1 + +int2;
+  if (Number.isNaN(int1) || Number.isNaN(int2)) {
+    throw new Error("Please check your input. Cannot sum non-numeric values");
+  }
+
+  return +int1 + +int2;
 };
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -1,29 +1,30 @@
-const {sum} = require('./calculator');
+const { sum } = require('./calculator');
 
+// deveria somar 2 e 2 e o resultado deve ser 4"
 it('should sum 2 and 2 and the result must be 4', () => {
-    expect(sum(2, 2)).toBe(4);
+  expect(sum(2, 2)).toBe(4);
 });
 
+// deveria somar 2 e 2, mesmo se um deles for uma string, o resultado deve ser 4"
 it('should sum 2 and 2 even if one of the is a string and the result must be 4', () => {
-    expect(sum('2', 2)).toBe(4);
+  expect(sum('2', 2)).toBe(4);
 });
 
+// deveria lançar um erro, se o que é fornecido ao método não pode ser somado
 it('should throw an error if what is provided to the method cannot be summed', () => {
-    expect(() => {
-        sum('', 2);
-    }).toThrowError();
-   
-    expect(() => {
-        sum([2, 2]);
-    }).toThrowError();
-    
-    expect(() => {
-        sum({});
-    }).toThrowError();
-    
-    expect(() => {
-        sum();
-    }).toThrowError();
-    
+  expect(() => {
+    sum('', 2);
+  }).toThrowError();
 
+  expect(() => {
+    sum([2, 2]);
+  }).toThrowError();
+
+  expect(() => {
+    sum({});
+  }).toThrowError();
+
+  expect(() => {
+    sum();
+  }).toThrowError();
 });
